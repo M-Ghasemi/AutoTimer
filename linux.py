5563b5decad0b0ae1a75c43711619e044278564f
Mohammad <hamp.cpas@gmail.com>
2019-09-06 00:05:36 +0430

    convert root.communicate returned bytes to string to make code compatible with python3 in linux platform

--- linux.py
+++ linux.py
@@ -1,46 +1,49 @@
-''' 
+"""
 This file gives the linux support for this repo
-'''
-import sys
-import os
+"""
 import subprocess
 import re
 
 
 def get_active_window_raw():
-    '''
-    returns the details about the window not just the title
-    '''
+    """returns the details about the window not just the title"""
     root = subprocess.Popen(
         ['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE)
     stdout, stderr = root.communicate()
+    if type(stdout) is bytes:
+        stdout = stdout.decode('utf-8')
 
-    m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout)
-    if m != None:
+    m = re.search('^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout)
+    if m is not None:
         window_id = m.group(1)
         window = subprocess.Popen(
             ['xprop', '-id', window_id, 'WM_NAME'], stdout=subprocess.PIPE)
         stdout, stderr = window.communicate()
+        if type(stdout) is bytes:
+            stdout = stdout.decode('utf-8')
     else:
         return None
 
-    match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
-    if match != None:
-        ret = match.group("name").strip(b'"')
-        #print(type(ret))
-        '''
+    match = re.match("WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
+    if match is not None:
+        ret = match.group("name").strip('"')
+        # print(type(ret))
+        """
         ret is str for python2
         ret is bytes for python3 (- gives error while calling in other file)
         be careful
-        '''
+        """
         return ret
     return None
 
-'''
+
+"""
 this file alone can be run without importing other files
 uncomment the below lines for linux - works - but activities won't be dumped in json file
 (may be it works for other OS also, not sure)
-'''
+"""
+
+
 # def run():
 #     new_window = None
 #     current_window = get_active_window_title()
@@ -54,19 +57,19 @@ uncomment the below lines for linux - works - but activities won't be dumped in 
 
 # run()
 def get_chrome_url_x():
-        ''' 
-        instead of url the name of the website and the title of the page is returned seperated by '/' 
-        '''
-        detail_full = get_active_window_raw()
-        detail_list = detail_full.split(' - ')
-        detail_list.pop()
-        detail_list = detail_list[::-1]
-        _active_window_name = 'Google Chrome -> ' + " / ".join(detail_list)
-        return _active_window_name
+    """
+    instead of url the name of the website and the title of the page is returned seperated by '/'
+    """
+    detail_full = get_active_window_raw()
+    detail_list = detail_full.split(' - ')
+    detail_list.pop()
+    detail_list = detail_list[::-1]
+    _active_window_name = 'Google Chrome -> ' + " / ".join(detail_list)
+    return _active_window_name
+
 
 def get_active_window_x():
     full_detail = get_active_window_raw()
     detail_list = None if None else full_detail.split(" - ")
     new_window_name = detail_list[-1]
     return new_window_name
-
